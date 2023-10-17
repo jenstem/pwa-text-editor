0e866da619300ccbeaa943638d4d12b8d25cc339
jenstem <124745888+jenstem@users.noreply.github.com>
2023-10-17 14:53:11 -0400

    Remove comments install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,9 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 });
-
 
 butInstall.addEventListener('click', async () => {
     const startEvent = window.deferredPrompt;
