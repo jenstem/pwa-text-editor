5520f64258d93347d946ac54a99356b03e3a4b2a
jenstem <124745888+jenstem@users.noreply.github.com>
2023-10-17 14:51:58 -0400

    Add click event handler on butInstall

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,3 +5,15 @@ window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 });
+
+
+butInstall.addEventListener('click', async () => {
+    const startEvent = window.deferredPrompt;
+    if (!startEvent) {
+        return;
+    }
+    startEvent.prompt();
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
